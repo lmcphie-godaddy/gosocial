2999f2c5d589327cc4ab60b1f30c0ecc24d77c8f
lalcantara-godaddy <lalcantara@godaddy.com>
2022-07-19 18:23:06 -0500

    w

--- sections/components/info.js
+++ sections/components/info.js
@@ -5,67 +5,59 @@ const info = () => {
     <div className={infoStyles.info}>
       <h1>Your Info</h1>
       <form>
-        <label
-          id="firstNameLabel"
-          htmlFor="firstName"
-          className={infoStyles.label}
-        >
-          First Name <span className={infoStyles.req}>*</span>
-        </label>
-        <input type="text" id="firstName" className={infoStyles.input}></input>
+        <div className={infoStyles.inputFields}>
+          <div className={infoStyles.firstLine}>
+            <div className={infoStyles.box}>
+              <label id="firstNameLabel" htmlFor="firstName">
+                First Name <span className={infoStyles.req}>*</span> <br></br>
+              </label>
+              <input type="text" id="firstName"></input>
+            </div>
+            <div className={infoStyles.firstLine}>
+              <label id="lastNameLabel" htmlFor="lastName">
+                Last Name
+                <span className={infoStyles.req}>*</span> <br></br>
+              </label>
+              <input type="text" id="lasttName"></input> <br></br>
+            </div>
+          </div>
+          <br></br>
+          <br></br>
+          <div className={infoStyles.firstLine}>
+            <div className={infoStyles.box}>
+              <label id="emailLabel" htmlFor="email">
+                Email
+                <span className={infoStyles.req}>*</span> <br></br>
+              </label>
+              <input type="text" id="email"></input>
+            </div>
 
-        <label
-          id="lastNameLabel"
-          htmlFor="lastName"
-          className={infoStyles.label}
-        >
-          Last Name
-          <span className={infoStyles.req}>*</span>
-        </label>
-        <input type="text" id="lasttName" className={infoStyles.input}></input>
+            <div className={infoStyles.box}>
+              <label id="phoneLabel" htmlFor="phone">
+                Phone Number
+                <span className={infoStyles.req}>*</span>
+                <br></br>
+              </label>
+              <input type="text" id="phone"></input>
+              <br></br>
+              <br></br>
+            </div>
+          </div>
 
-        <br></br>
-        <br></br>
+          <label id="companyLabel" htmlFor="company">
+            Company Name
+            <span className={infoStyles.req}>*</span>
+          </label>
+          <input type="text" id="company"></input>
+          <br></br>
+          <br></br>
 
-        <label id="emailLabel" htmlFor="email">
-          Email
-          <span className={infoStyles.req}>*</span>
-        </label>
-        <input type="text" id="email" className={infoStyles.input}></input>
-
-        <label id="phoneLabel" htmlFor="phone">
-          Phone Number
-          <span className={infoStyles.req}>*</span>
-        </label>
-        <input type="text" id="phone" className={infoStyles.input}></input>
-        <br></br>
-        <br></br>
-
-        <label id="companyLabel" htmlFor="company" className={infoStyles.label}>
-          Company Name
-          <span className={infoStyles.req}>*</span>
-        </label>
-        <input
-          type="text"
-          id="company"
-          className={infoStyles.input_bottom}
-        ></input>
-        <br></br>
-        <br></br>
-
-        <label
-          id="companyAddressLabel"
-          htmlFor="companyAddress"
-          className={infoStyles.label}
-        >
-          Company Address
-          <span className={infoStyles.req}>*</span>
-        </label>
-        <input
-          type="text"
-          id="companyAddress"
-          className={infoStyles.input_bottom}
-        ></input>
+          <label id="companyAddressLabel" htmlFor="companyAddress">
+            Company Address
+            <span className={infoStyles.req}>*</span>
+          </label>
+          <input type="text" id="companyAddress"></input>
+        </div>
       </form>
     </div>
   );
