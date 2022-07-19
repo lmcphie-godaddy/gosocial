8c5ad85ee28708063320e90f6cf022a1703a99d4
shusain-godaddy <shusain@godaddy.com>
2022-07-19 15:06:23 -0400

    sumayyah set up

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Head from '../components/head';
-import GasketEmblem from '@gasket/assets/react/gasket-emblem';
+import Header from '../sections/components/header.js';
 
-const pageStyle = { textAlign: 'center' };
-const logoStyle = { width: '250px', height: '250px' };
-
-export const IndexPage = () => (
-  <div style={ pageStyle }>
-    <Head title='Home'/>
-    <GasketEmblem style={ logoStyle }/>
-    <h1>Welcome to Gasket!</h1>
-    <p>To get started, edit <code>pages/index.js</code> and save to reload.</p>
-    <p><a href='https://gasket.dev'>Learn Gasket</a></p>
-  </div>
-);
+const IndexPage = () => {
+  return(
+      <>
+      <Head>
+        <title>GoSocial</title>
+      </Head>
+      <Header/>
+      
+      </>
+  )
+  };
 
 export default IndexPage;
