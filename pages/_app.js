f1d9b5791c8c04f7557f9921fad171d94527caae
lalcantara-godaddy <lalcantara@godaddy.com>
2022-07-19 22:37:27 -0500

    latest

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 const { nextRedux } = require("../redux/store");
+import "@ux/modal/dist/styles.css";
+import "@ux/Button/dist/styles.css";
+import "../styles/global.css";
+import "@ux/icon/download/index.css";
+import "@ux/growl/dist/styles.css";
 
 // Simple functional App component which can be wrapped
 function WrappedApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,27 @@ import Payment from "../sections/components/payment.js";
 import YourInfo from "../sections/components/yourInfo.js";
 import styles from "../styles/pages/index.module.css";
 import Item from "../sections/components/items.js";
-
+import Modal from "@ux/modal";
+import Button from "@ux/button";
+import Download from "@ux/icon/download";
 
 const sendEmail = () => {
   fetch("http://localhost:9002")
-  .then(res => {console.log("success")}).catch(err => console.log(err))
+    .then((res) => {
+      console.log("success");
+    })
+    .catch((err) => console.log(err));
 };
 
 const IndexPage = () => {
-  return ( <>
+  const [show, setShow] = React.useState(false);
+
+  function myFunction() {
+    alert("Success, check your inbox!");
+  }
+
+  return (
+    <>
       <Header />
       <div className={styles.background}>
         <div className={styles.wrapper}>
@@ -22,12 +34,50 @@ const IndexPage = () => {
           <Item />
           <Payment />
           <div className={styles.completeButton}>
-            <button type="button" onClick={sendEmail}>Complete Checkout</button>
+            <Button
+              type="button"
+              design="primary"
+              onClick={() => {
+                sendEmail();
+                setShow(true);
+              }}
+            >
+              Complete Checkout
+            </Button>
+            {show && (
+              <Modal
+                id="default-example"
+                title=""
+                onClose={() => setShow(false)}
+              >
+                <h2 className={styles.Modal}>Congratulations!</h2>
+
+                <h3>You just bought your first domain!</h3>
+
+                <div>
+                  We’ve created a gift just for you. Be proud and share with
+                  your community! Download below or check your email for a
+                  special surprise.
+                </div>
+                <br></br>
+
+                <Button
+                  type="button"
+                  design="primary"
+                  onClick={() => {
+                    myFunction();
+                    setShow(false);
+                  }}
+                >
+                  <Download />
+                </Button>
+              </Modal>
+            )}
           </div>
         </div>
       </div>
     </>
-  )
+  );
 };
 
 export default IndexPage;
