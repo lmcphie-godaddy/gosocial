94eedb6cc051ec857c97a05c54d9bde7758e1bcc
shusain-godaddy <shusain@godaddy.com>
2022-07-19 20:50:01 -0400

    entire page layout updated/all 3 cards

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from '../components/head';
 import Header from '../sections/components/header.js';
 import Payment from '../sections/components/payment.js';
 import YourInfo from '../sections/components/yourInfo.js';
+import Item from '../sections/components/items.js';
 import styles from '../styles/pages/index.module.css';
 
 const IndexPage = () => {
@@ -15,8 +16,12 @@ const IndexPage = () => {
 
       <Header/>
       <div className={styles.background}>
+        <div className={styles.wrapper}>
         <YourInfo/>
+        <Item/>
         <Payment/>
+        </div>
+        
       </div>
       </>
   )
---
+++ sections/components/items.js
@@ -0,0 +1,11 @@
+import itemStyles from "../../styles/items.module.css";
+
+const Items = () => {
+    return (
+        <div className={itemStyles.Items}>
+            <h1>Your Items</h1>
+        </div>
+    )
+};
+
+export default Items;
